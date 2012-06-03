6403b2241af9322659e3b84c29ace3ec76c6c04d
cacciatc <chris.cacciatore@gmail.com>
2012-06-03 15:18:46 -0700

    merged hi and hedgehog

--- hedgehog.rb
+++ hedgehog.rb
@@ -5,6 +5,8 @@ require 'dm-core'
 require 'dm-migrations'
 require 'dm-aggregates'
 require 'dm-constraints'
+require 'uri'
+require 'net/http'
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   [username, password] == ['fullhouse', 'tanners']
@@ -19,7 +21,6 @@ end
 
 configure do
 	DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:///#{Dir.pwd}/db/test.db")
-	
 	class TextMessages
 		include DataMapper::Resource
 	
@@ -33,6 +34,25 @@ helpers do
 	require settings.root + '/helper'
 end
 
-get '/' do
-	erb :index
+# routes
+get "/" do	
+	erb :index2
+end
+
+get "/landing" do
+        erb :landing
+end
+
+get "/channel" do
+        "<script src='//connect.facebook.net/en_US/all.js'></script>"
+end
+
+#Send a POST request to Facebook's REST API event message poster.
+get '/eventMessagePost/:accessToken/:eventId/:message' do
+        args = {
+                'access_token' =>  params[:accessToken] ,
+                'message' =>  params[:message]
+        }
+        puts URI.parse("https://graph.facebook.com/#{params[:eventId]}/feed")
+        Net::HTTP.post_form(URI.parse("https://graph.facebook.com/#{params[:eventId]}/feed"), args)
 end
