e5f1a8284c7ea3fa9969f892d8f476a2692f9616
Salem Hilal <salemhilal@gmail.com>
2012-06-03 02:53:00 -0700

    Added functions to send cross domain POST requests.

--- hi.rb
+++ hi.rb
@@ -1,31 +1,40 @@
 require 'sinatra'
 require 'oauth2'
 require 'json'
-#class App < Sinatra::Base
+require 'uri'
+require 'net/http'
 
-  get "/" do
-    erb :index2
-  end
+get "/" do
+erb :index2
+end
 
-  get "/landing" do
-  	erb :landing
-  end
+get "/landing" do
+	erb :landing
+end
 
-  get "/channel" do
-  	"<script src='//connect.facebook.net/en_US/all.js'></script>"
-  end
+get "/channel" do
+	"<script src='//connect.facebook.net/en_US/all.js'></script>"
+end
 
-  get '/auth/facebook' do
-    redirect client.auth_code.authorize_url(
-      :redirect_uri => redirect_uri,
-      :scope => 'email'
-    )
-  end
+#Send a POST request to Facebook's REST API event message poster.
+get '/eventMessagePost/:accessToken/:eventId/:message' do
+	params = {
+		'access_token' => :accessToken,
+		'message' => :message
+	}
+	Net::HTTP.post_form(URI.parse('http://graph.facebook.com/' + :eventId + "/feed"), params)
+end
 
-  get '/auth/facebook/callback' do
-    token = client.auth_code.get_token(@data[:code], {:redirect_uri => redirect_uri, :parsed => :facebook})
-    user = token.get('/me').parsed
-    create_user user unless user_exists user
-  end
-
-
+#Send a POST request to Facebook's REST API event creater.
+get '/eventCreate/:accessToken/:userId/:name/:startTime/:endTime/:location/:description' do
+	params = {
+		'access_token' => :user,
+		'name' => :name,
+		'start_time' => :startTime,
+		'end_time' => :endTime,
+		'location' => :location,
+		'description' => :description,
+		'privacy' => 'SECRET'
+	}
+	Net::HTTP.post_form(URI.parse('http://graph.facebook.com/' + :userId + "/events"), params)
+end
